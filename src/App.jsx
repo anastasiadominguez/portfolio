c7bb208dc5082a32ca56ecf4ffa6087822648ce0
anastasiadominguez <s319934@studenti.polito.it>
2025-05-02 16:50:38 +0200

    homepage setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "/images/logoADB.png";
+import photo from "/images/photo.JPG";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container">
+        <div className="top-bar">
+          <div className="logo">
+            <img src={logo} className="logo" alt="Vite logo" />
+          </div>
+          <div className="link">
+            <div className="link-item">Home</div>
+            <div className="link-item">Chi sono</div>
+            <div className="link-item">Progetti</div>
+            <div className="link-item">Contatti</div>
+          </div>
+        </div>
+        <div className="intro">
+          <div className="intro-text">Anastasia Dominguez Burzio</div>
+          <div className="intro-image">
+            <img src={photo} alt="Intro" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
